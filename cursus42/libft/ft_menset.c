cea85d9ce06eb6f0a47276eb0bf63a520299d43d
cristiancarrero <161723801+cristiancarrero@users.noreply.github.com>
2024-03-11 17:19:05 +0100

    Add files via upload

---
+++ cursus42/libft/ft_menset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_menset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 15:50:32 by ccarrero          #+#    #+#             */
+/*   Updated: 2024/03/11 17:03:13 by ccarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memset(void *str, int c, size_t n)
+{
+	unsigned char	*p;
+	size_t			i;
+
+	i = 0;
+	p = (unsigned char *)str;
+	while (i < n)
+	{
+		*p = (unsigned char)c;
+		p++;
+		i++;
+	}
+	return (str);
+}
