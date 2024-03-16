adcd1b53f0f0cdb455a7525a198b02767ba40ead
cristiancarrero <161723801+cristiancarrero@users.noreply.github.com>
2024-03-16 01:07:50 +0100

    Add files via upload

---
+++ cursus42/libft/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 01:05:09 by marvin            #+#    #+#             */
+/*   Updated: 2024/03/16 01:05:09 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_calloc(size_t n, size_t size)
+{
+	void	*buffer;
+
+	buffer = (void *)malloc(size * n);
+	if (!buffer)
+		return (NULL);
+	ft_bzero(buffer, size * n);
+	return (buffer);
+}
