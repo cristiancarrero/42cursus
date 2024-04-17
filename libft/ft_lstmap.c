ad2d25c28e840e03363c1e112fbdd98f52d8a2a1
cristiancarrero <161723801+cristiancarrero@users.noreply.github.com>
2024-04-18 01:14:39 +0200

    Add files via upload

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarrero <ccarrero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 13:41:18 by ccarrero          #+#    #+#             */
+/*   Updated: 2024/04/01 16:39:37 by ccarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	void	*content;
+	t_list	*new_node;
+
+	new_lst = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		if (!content)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		new_node = ft_lstnew(content);
+		if (!new_node)
+		{
+			ft_lstclear(&new_lst, del);
+			del(content);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
