06565dfbd8a2760f7532ae7b97848252b5ab2c89
cristiancarrero <161723801+cristiancarrero@users.noreply.github.com>
2024-04-24 03:58:35 +0200

    Update ft_striteri.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -25,3 +25,19 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		i++;
 	}
 }
+/*
+void print_index_char(unsigned int index, char *c)
+{
+    printf("Index: %u, Character: %c\n", index, *c);
+}
+
+int main(void)
+{
+    char str[] = "Hello";
+
+    printf("Original string: %s\n", str);
+
+    ft_striteri(str, print_index_char);
+
+    return 0;
+}*/
